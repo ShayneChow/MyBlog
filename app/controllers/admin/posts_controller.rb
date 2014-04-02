1e1d1ab988c89b2808693dca27c4950e461a2155
ShayneChow <shayne@authbuy.org>
2014-04-02 17:16:52 +0800

    完成post删除操作，修正后台layout的js加载方式，改为application

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -20,6 +20,10 @@ class Admin::PostsController < Admin::ApplicationController
     end
   end
 
+  #def show
+    #@post = Post.find params[:id]
+  #end
+
   #编辑Blog
   def edit
     @post = Post.find params[:id]
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
- # validates_presence_of :title, :content
+  #validates_presence_of :title, :content
+
+  validates :title, :presence => true, :length => { :minimum => 5 }
+  validates :content, :desc, :presence => true
 
 end
