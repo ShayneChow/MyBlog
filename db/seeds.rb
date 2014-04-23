387cf6af885f97fb271aa949e58fc3ab508de162
ShayneChow <shayne@authbuy.org>
2014-04-23 16:27:17 +0800

    修改seed文件，预写管理员账号

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create name:"ShayneChow", email:"shayne@authbuy.org", password:"Google123"
+User.create name:"ShayneChow", email:"shayne@chow.com", password:"Google123"
