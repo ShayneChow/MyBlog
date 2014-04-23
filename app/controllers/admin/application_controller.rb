764642bf0b4490875e30fd1044cf33e1afd436e9
ShayneChow <shayne@authbuy.org>
2014-04-23 16:23:54 +0800

    后台加登录限制

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,22 @@
 class Admin::ApplicationController < ::ApplicationController
   layout 'admin'
+
+  include Admin::ApplicationHelper
+
+  # 后台控制器的初始化操作
+  #before_filter :init
+  before_filter :check_auth
+
+  protected
+  # 初始化控制器
+  #def init
+  #end
+
+  # 检查权限
+  def check_auth
+    if !current_user && params[:controller]!='admin/sessions'
+      redirect_to admin_login_url
+    end
+  end
+
 end
